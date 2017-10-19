ffcc865226ca32e368edeb7270f4c175a26eeb50
bjarte <bjartejensen@gmail.com>
2017-10-19 15:29:55 +0200

    Including styles

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  template: '<app-server></app-server><app-server></app-server>',
+  // styleUrls: ['./servers.component.css'],
+  styles: [`
+      h3 {color: rebeccapurple;}
+  `]
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
